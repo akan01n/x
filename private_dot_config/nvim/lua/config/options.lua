fc7f1797d1dd6f2e40aec77c48ae6d747654864a
Davi R <2388371+akan01n@users.noreply.github.com>
2025-03-04 12:56:41 -0300

    -

---
+++ private_dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- fix copilot suggestions
+vim.g.ai_cmp = false
+
+-- disable animations
+vim.g.snacks_animate = false
